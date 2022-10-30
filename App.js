cc78fea3ed71223caf2196152fe9c2b42088150e
Mikkel Ofrim <39702459+Mikkelof@users.noreply.github.com>
2022-10-31 00:03:45 +0100

    Pretty much done, just need some small design changes

--- App.js
+++ App.js
@@ -3,11 +3,38 @@ import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import ListList from './components/ListList';
 import ItemList from './components/ItemList';
+import * as FileSystem from 'expo-file-system';
 
 export default function App() {
   const [modalIsVisible, setModalIsVisible] = useState(true);
-  const [listItems, setListItems] = useState([]);
-  const [listTitles, setListTitles] = useState([]);
+  const [listItems, setListItems] = useState(async () => {
+    const itemsFilename = FileSystem.documentDirectory + "items.json";
+    try {
+      const itemsJson = await FileSystem.readAsStringAsync(itemsFilename);
+      const items = JSON.parse(itemsJson);
+      // for(var i in items) {
+      //   itemList.push([i, items[i]]);
+      // }
+      setListItems(items);
+      return items;
+    } catch(e) {
+      console.log(e);
+    }
+  });
+  const [listTitles, setListTitles] = useState(async () => {
+    const titlesFilename = FileSystem.documentDirectory + "titles.json";
+    try {
+      const titlesJson = await FileSystem.readAsStringAsync(titlesFilename);
+      const titles = JSON.parse(titlesJson);
+      // for(var i in titles) {
+      //   titleList.push([i, titles[i]]);
+      // }
+      setListTitles(titles);
+      return titles;
+    } catch(e) {
+      console.log(e);
+    }
+  });
   const [currentListItems, setCurrentListItems] = useState([]);
   const [currentListId, setCurrentListId] = useState();
   const [currentTitle, setCurrentTitle] = useState('');
@@ -17,8 +44,67 @@ export default function App() {
   }
 
   useEffect(() => {
-    setCurrentListItems(getFilteredList(currentListId));
-  }, [currentListId, listItems])
+    if(typeof currentListId !== "undefined") {
+      setCurrentListItems(getFilteredList(currentListId));
+    }
+  }, [currentListId, listItems, listTitles])
+
+  useEffect(() => {
+    const itemsFilename = FileSystem.documentDirectory + "items.json";
+    FileSystem.writeAsStringAsync(itemsFilename, JSON.stringify(listItems));
+  }, [listItems, currentListItems])
+
+  useEffect(() => {
+    const titlesFilename = FileSystem.documentDirectory + "titles.json";
+    FileSystem.writeAsStringAsync(titlesFilename, JSON.stringify(listTitles));
+  }, [listTitles])
+
+  // useEffect(() => {
+  //   fetchItems();
+  //   fetchTitles();
+  //   console.log(Array.isArray(oldItems));
+  //   setListItems(oldItems);
+  //   setListTitles(oldTitles);
+  // }, [])
+
+  // async function fetchItems() {
+  //   const itemsFilename = FileSystem.documentDirectory + "items.json";
+  //   try {
+  //     const itemList = [];
+  //     const itemsJson = await FileSystem.readAsStringAsync(itemsFilename);
+  //     const items = JSON.parse(itemsJson);
+  //     console.log(items);
+  //     for(var i in items) {
+  //       itemList.push([i, items[i]]);
+  //       console.log(itemList.length);
+  //     }
+  //     setListItems(itemList);
+  //     console.log(Array.isArray(itemList));
+  //     return itemList;
+  //   } catch(e) {
+  //     console.log(e);
+  //   }
+  // }
+
+  // async function fetchTitles() {
+  //   const titlesFilename = FileSystem.documentDirectory + "titles.json";
+  //   try {
+  //     var titleList = [];
+  //     const titlesJson = await FileSystem.readAsStringAsync(titlesFilename);
+  //     const titles = JSON.parse(titlesJson);
+  //     console.log(titles)
+  //     for(var i in titles) {
+  //       titleList.push([i, titles[i]]);
+  //       console.log(titleList.length);
+  //     }
+  //     setListTitles(titleList);
+  //     console.log(titlesJson)
+  //     console.log(Array.isArray(titleList));
+  //     return titleList;
+  //   } catch(e) {
+  //     console.log(e);
+  //   }
+  // }
 
   //To add an item to an existing list
   function addListItemHandler(enteredListItemText) {
